9b7d54e997c25504a119535ff87c0a181c69f538
rita <AdaRitDN@mail.ru>
2023-01-12 12:56:06 +0300

    выходной и будний день

---
+++ qwerty3/Program.cs
@@ -0,0 +1,14 @@
+﻿//15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+
+ Console.Write("Номер дня");
+ int day = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(day % 7);
+if (day % 7 > 0 & day % 7 <= 5)
+{ 
+  Console.WriteLine("Будний день");
+}
+ else
+{
+  Console.WriteLine("Выходной день");
+}
