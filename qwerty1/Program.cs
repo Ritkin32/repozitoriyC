284cb0f8002c4d5ce8810fe966b43b171df20898
rita <AdaRitDN@mail.ru>
2023-01-12 09:26:47 +0300

    блок схема и код

--- qwerty1/Program.cs
+++ qwerty1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (100 <= number && number <= 999)
+{
+  number /= 10;
+  number = number % 10;
+  Console.WriteLine(number);
+}
+else
+{
+Console.WriteLine("Нужно ввести число");
+}
