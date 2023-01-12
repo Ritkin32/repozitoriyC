7839f3c9e01f549632383d9a4a184743597b8c3f
rita <AdaRitDN@mail.ru>
2023-01-12 09:56:14 +0300

    код нахождение 3 числа

---
+++ qwerty2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+  Console.WriteLine((number % 10));
+}
+else
+{
+if (number >99)
+    number = number / 10;
+Console.Write("третьей цифры нет");    
+}
