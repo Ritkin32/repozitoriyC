13f1b55d35b5693357669281a9fe4aea788aace1
rita <AdaRitDN@mail.ru>
2023-01-14 10:34:40 +0300

    фибоначчи

---
+++ qwerty81/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите N");
+int val2 = 0;
+while (val1 != 0)
+{
+ int o = val1 % 10;
+ val1 =val1 / 10;
+ val2 = val2 * 10 + o;
+}
+Console.WriteLine(val2);
+Console.WriteLine(t == val2);
